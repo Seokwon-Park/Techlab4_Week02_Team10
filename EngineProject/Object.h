0efa9c95c6f3ada0acb3f84dec0af32bd62a66fb
Seokwon Park <rty3394@naver.com>
2026-09-05 10:03:56 +0900

    Fix: 어제 병합 과정에서 Project에서 제외된 파일 다시 추가

--- EngineProject/Object.h
+++ EngineProject/Object.h
@@ -15,7 +15,7 @@ public:                                                                 \
         if (!bIsInit)                                                   \
         {                                                               \
             c.Name  = #ClassName;                                       \
-            c.Super = SuperClassName::StaticClass();                    \
+            c.Super = Super::StaticClass();								\
             c.Constructor = []() -> UObject* { return new ClassName(); };\
             bIsInit = true;                                             \
         }                                                               \
