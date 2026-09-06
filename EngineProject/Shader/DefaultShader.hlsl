05eb551154438ea5934204d988eadf7f8f8ba0b9
HYEJIN HWANG <itree1030@gmail.com>
2026-09-07 00:50:49 +0900

    feat: 기본 셰이더 코드에 MVP 행렬 곱 추가

--- EngineProject/Shader/DefaultShader.hlsl
+++ EngineProject/Shader/DefaultShader.hlsl
@@ -18,8 +18,8 @@ struct PS_INPUT
 PS_INPUT mainVS(VS_INPUT input)
 {
 	PS_INPUT output;
-	
-	output.position = float4(input.position, 1.0f);
+    
+    output.position = mul(float4(input.position, 1.0f), MVP);
 	output.color = input.color;
 	return output;
 }
