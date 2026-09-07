eeef473664b39789fc6c72642b17964dd45fe79e
HYEJIN HWANG <itree1030@gmail.com>
2026-09-07 21:19:20 +0900

    fix: DirectX 전방선언 class 에서 struct 로 변경

--- EngineProject/Shader.h
+++ EngineProject/Shader.h
@@ -2,9 +2,9 @@
 #include <wrl/client.h>
 #include "Types.h"
 
-class ID3D11VertexShader;
-class ID3D11PixelShader;
-class ID3D11InputLayout;
+struct ID3D11VertexShader;
+struct ID3D11PixelShader;
+struct ID3D11InputLayout;
 
 enum class EShaderType
 {
